00a9c4e33aa6bd3def765234bf22c3a324c2acb5
Maja Kijewska <maja@MacBook-Air-Maja-2.local>
2026-01-04 14:21:30 +0100

    humanA class created

--- ex03/HumanA.hpp
+++ ex03/HumanA.hpp
@@ -6,7 +6,27 @@
 /*   By: majkijew <majkijew@student.42heilbronn.de> +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/03 19:31:44 by majkijew          #+#    #+#             */
-/*   Updated: 2026/01/03 19:31:45 by majkijew         ###   ########.fr       */
+/*   Updated: 2026/01/04 14:15:54 by majkijew         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef HUMANA_HPP
+# define HUMANA_HPP
+
+#include "Weapon.hpp"
+
+class HumanA{
+    private: 
+        std::string _name;
+        Weapon      _type;
+    public:
+        HumanA();
+        ~HumanA();
+        void setName( std::string name);
+        std::string getName();
+        void setWeapon( Weapon type );
+        std::string getWeapon( );
+        void attack();
+};
+
+#endif

48c4a5d84e6a0060738c889a274fc835aa79ee71
Maja Kijewska <maja@MacBook-Air-Maja-2.local>
2026-01-04 14:20:43 +0100

    weapon class created

--- ex03/Weapon.cpp
+++ ex03/Weapon.cpp
@@ -6,7 +6,19 @@
 /*   By: majkijew <majkijew@student.42heilbronn.de> +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/03 19:31:31 by majkijew          #+#    #+#             */
-/*   Updated: 2026/01/03 19:31:32 by majkijew         ###   ########.fr       */
+/*   Updated: 2026/01/04 13:45:47 by majkijew         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "Weapon.hpp"
+
+Weapon::Weapon() : _type("") {}
+
+Weapon::~Weapon() {}
+
+void	Weapon::setType(std::string type){
+    _type = type;
+}
+std::string	Weapon::getType(){
+    return (_type);    
+}
--- ex03/Weapon.hpp
+++ ex03/Weapon.hpp
@@ -6,14 +6,22 @@
 /*   By: majkijew <majkijew@student.42heilbronn.de> +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/03 19:31:16 by majkijew          #+#    #+#             */
-/*   Updated: 2026/01/03 19:34:51 by majkijew         ###   ########.fr       */
+/*   Updated: 2026/01/04 13:52:27 by majkijew         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef WEAPON_HPP
+# define WEAPON_HPP
 #include <iostream>
 
-class Weapon() {
+class Weapon {
 	private:
-		std::string
+		std::string _type;
 	public:
+		Weapon();
+		~Weapon();
+		void	setType(std::string type);
+		std::string	getType();
 };
+
+#endif

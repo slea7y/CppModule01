ff096bb7b5a91aa428186ce11669f0a749300f36
Maja Kijewska <maja@MacBook-Air-Maja-2.local>
2026-01-08 13:54:05 +0100

    harl class

--- ex05/Harl.cpp
+++ ex05/Harl.cpp
@@ -6,7 +6,31 @@
 /*   By: majkijew <majkijew@student.42heilbronn.de> +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/05 17:17:51 by majkijew          #+#    #+#             */
-/*   Updated: 2026/01/05 17:17:52 by majkijew         ###   ########.fr       */
+/*   Updated: 2026/01/05 19:39:42 by majkijew         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "Harl.hpp"
+
+void Harl::debug( void ) {
+	std::cout << "I love having extra bacon for my 7XL-double-cheese-triple-pickle-special- ketchup burger. I really do!\n";
+}
+
+void Harl::info( void ) {
+	std::cout << "I cannot believe adding extra bacon costs more money. You didn’t put enough bacon in my burger! If you did, I wouldn’t be asking for more!\n";
+}
+
+void Harl::warning( void ) {
+	std::cout << "I think I deserve to have some extra bacon for free. I’ve been coming for years, whereas you started working here just last month.\n";
+}
+
+void Harl::error( void ) {
+	std::cout << "This is unacceptable! I want to speak to the manager now.\n";
+}
+
+void Harl::complain( std::string level ) {
+	debug();
+	info();
+	warning();
+	error();
+}
--- ex05/Harl.hpp
+++ ex05/Harl.hpp
@@ -6,7 +6,29 @@
 /*   By: majkijew <majkijew@student.42heilbronn.de> +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/05 17:17:42 by majkijew          #+#    #+#             */
-/*   Updated: 2026/01/05 17:17:43 by majkijew         ###   ########.fr       */
+/*   Updated: 2026/01/05 19:26:16 by majkijew         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef HARL_HPP
+# define HARL_HPP
+
+#include <iostream>
+
+class Harl {
+    private:
+        void debug( void );
+        void info( void );
+        void warning( void );
+        void error( void );
+    public:
+        Harl(/* args */);
+        ~Harl();
+        void    complain( std::string level );
+};
+
+Harl::Harl(){}
+
+Harl::~Harl() {}
+
+#endif
